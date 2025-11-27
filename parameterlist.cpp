09a09e5d0bc8f16e001352d995df37f085672e6a
szamaro <firewall606@gmail.com>
2025-11-27 10:22:17 +0300

    fix bug with syncs. search added

--- parameterlist.cpp
+++ parameterlist.cpp
@@ -1,16 +1,60 @@
 #include "parameterlist.h"
 #include "ui_parameterlist.h"
 
+#include <QFile>
+#include <QRegularExpression>
+#include <QRegularExpressionMatch>
+
 #include <common/mavlink.h>
+
+Parameter::Parameter(QTableWidgetItem* nameItem_, QTableWidgetItem* valueItem_, QTableWidgetItem* typeIdItem_, QTableWidgetItem* typeItem_)
+    : nameItem(nameItem_)
+    , valueItem(valueItem_)
+    , typeIdItem(typeIdItem_)
+    , typeItem(typeIdItem_)
+{
+    name = nameItem ? nameItem->text() : "";
+    value = valueItem ? valueItem->text() : "";
+    typeId = typeIdItem ? typeIdItem->text() : "";
+    type = typeItem ? typeItem->text() : "";
+}
+
+Parameter::Parameter(QString name_, QString value_, QString typeId_, QString type_)
+    : name(name_)
+    , value(value_)
+    , typeId(typeId_)
+    , type(type_)
+{
+    //nameItem = new QTableWidgetItem(name);
+    //valueItem = new QTableWidgetItem(value);
+    //typeIdItem = new QTableWidgetItem(typeId);
+    //typeItem = new QTableWidgetItem(type);
+}
+
+void ParameterList::hideAll() {
+    for(size_t i = 0; i < ui->parameterList->rowCount(); ++i) {
+        if(!ui->parameterList->isRowHidden(i))
+            ui->parameterList->hideRow(i);
+    }
+}
+
+void ParameterList::showAll() {
+    for(size_t i = 0; i < ui->parameterList->rowCount(); ++i) {
+        if(ui->parameterList->isRowHidden(i))
+            ui->parameterList->showRow(i);
+    }
+}
 
 ParameterList::ParameterList(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::ParameterList)
 {
     ui->setupUi(this);
+    _buffer = new QTableWidget();
     connect(ui->parameterList, &QTableWidget::itemChanged, this, [this](QTableWidgetItem* item){
         setSingleParameterRequested(item->row());
     });
+    //ui->parameterList->sortItems(0);
 }
 
 ParameterList::~ParameterList()
@@ -24,7 +68,12 @@ void ParameterList::onAutopilotHeartbeat(const mavlink_message_t& msg) {
 }
 
 void ParameterList::handleMavlink(const mavlink_param_value_t& msg) {
-    QList<QTableWidgetItem*> items = ui->parameterList->findItems(msg.param_id, Qt::MatchExactly);
+    QByteArray msgParamId(msg.param_id, 16);
+    int nullIndex = msgParamId.indexOf('\0');
+    if (nullIndex >= 0) {
+            msgParamId.truncate(nullIndex);
+    }
+    QList<QTableWidgetItem*> items = ui->parameterList->findItems(msgParamId, Qt::MatchExactly);
     QTableWidgetItem* valueItemToSet = nullptr;
     ui->parameterList->blockSignals(true);
     if(!items.empty()) {
@@ -50,7 +99,7 @@ void ParameterList::handleMavlink(const mavlink_param_value_t& msg) {
     else {
         ui->parameterList->insertRow(ui->parameterList->rowCount());
 
-        QTableWidgetItem* paramNameItem = new QTableWidgetItem(QString(msg.param_id).trimmed());
+        QTableWidgetItem* paramNameItem = new QTableWidgetItem(msgParamId);
         paramNameItem->setFlags(paramNameItem->flags() & ~Qt::ItemIsEditable);
         ui->parameterList->setItem(ui->parameterList->rowCount()-1, 0, paramNameItem);
 
@@ -191,6 +240,55 @@ void ParameterList::setAllParametersRequested() {
     }
 }
 
+QString ParameterList::serializeParameter(size_t rowIndex) {
+    Parameter parameter(
+        ui->parameterList->item(rowIndex, 0),
+        ui->parameterList->item(rowIndex, 1),
+        ui->parameterList->item(rowIndex, 2),
+        ui->parameterList->item(rowIndex, 3)
+    );
+    return parameter.name + "\t" + parameter.value;
+}
+
+void ParameterList::saveToFile(const QString& path) {
+    QFile file(path);
+        if (!file.open(QIODevice::WriteOnly | QIODevice::Text)) {
+            qDebug() << "Cannot open file for writing:" << file.errorString();
+            return;
+        }
+
+        QTextStream stream(&file);
+        stream.setEncoding(QStringConverter::Utf8);
+
+        for(int i = 0; i < ui->parameterList->rowCount(); ++i) {
+            QString line = serializeParameter(i);
+            if (!line.isEmpty()) {
+                stream << line << "\n";
+            }
+
+            // Проверяем ошибки после каждой записи
+            if (stream.status() != QTextStream::Ok) {
+                qDebug() << "Stream error at parameter" << i << ":" << stream.status();
+                break;
+            }
+
+            // Принудительно сбрасываем буфер каждые 100 записей
+            if (i % 100 == 0) {
+                stream.flush();
+                if (file.error() != QFile::NoError) {
+                    qDebug() << "File error at parameter" << i << ":" << file.errorString();
+                    break;
+                }
+            }
+        }
+
+        stream.flush();
+        file.close();
+
+        qDebug() << "Total parameters processed:" << ui->parameterList->rowCount();
+        qDebug() << "File size:" << file.size() << "bytes";
+}
+
 void ParameterList::on_syncVehicleWithUs_clicked()
 {
     setAllParametersRequested();
@@ -202,5 +300,51 @@ void ParameterList::on_syncUsWithVehicle_clicked()
     mavlink_message_t msg;
     mavlink_msg_param_request_list_pack(255, MAV_COMP_ID_MISSIONPLANNER, &msg, _sysId, MAV_COMP_ID_AUTOPILOT1);
     emit parametersRequest(msg);
+}
+
+void ParameterList::on_saveToFileButton_clicked()
+{
+    ui->parameterList->sortItems(0);
+    saveToFile("/home/szamaro/Projects/CGS/parameterSet.param");
+}
+
+
+void ParameterList::on_loadFromFileButton_clicked()
+{
+    QFile file("/home/szamaro/Projects/CGS/parameterSet.param");
+    file.open(QIODevice::ReadOnly);
+    if(file.isOpen()) {
+        ui->parameterList->blockSignals(true);
+        while(file.isReadable()) {
+            QString line = file.readLine();
+            line = line.trimmed();
+            if(line.size() < 1) break;
+            QStringList list = line.split(QRegularExpression("\\s+"));
+
+            Parameter parameter(list[0], list[1]);
+            QList<QTableWidgetItem*> items = ui->parameterList->findItems(parameter.name, Qt::MatchExactly);
+            if(!items.empty()) {
+                ui->parameterList->item(items[0]->row(), 1)->setText(parameter.value);
+            }
+            else {
+                ui->parameterList->insertRow(ui->parameterList->rowCount());
+                QTableWidgetItem* nameItem = new QTableWidgetItem(parameter.name);
+                ui->parameterList->setItem(ui->parameterList->rowCount() - 1, 0, nameItem);
+                ui->parameterList->setItem(nameItem->row(), 1, new QTableWidgetItem(parameter.value));
+            }
+        }
+        ui->parameterList->blockSignals(false);
+    }
+}
+
+
+void ParameterList::on_lineEdit_textChanged(const QString &arg1)
+{
+    if(arg1 == "") { showAll(); return; }
+    hideAll();
+    QList<QTableWidgetItem*> matches = ui->parameterList->findItems(arg1, Qt::MatchRegularExpression);
+    for(size_t i = 0; i < matches.size(); ++i) {
+        ui->parameterList->showRow(matches[i]->row());
+    }
 }
 
--- parameterlist.h
+++ parameterlist.h
@@ -22,12 +22,35 @@ typedef __mavlink_param_request_list_t mavlink_param_request_list_t;
 struct __mavlink_message;
 typedef __mavlink_message mavlink_message_t;
 
+class QTableWidgetItem;
+class QTableWidget;
+
+struct Parameter {
+    QTableWidgetItem* nameItem;
+    QTableWidgetItem* valueItem;
+    QTableWidgetItem* typeIdItem;
+    QTableWidgetItem* typeItem;
+
+    Parameter(QTableWidgetItem* nameItem_, QTableWidgetItem* valueItem_, QTableWidgetItem* typeIdItem_, QTableWidgetItem* typeItem_);
+    Parameter(QString name_, QString value_, QString typeId_ = "", QString type_ = "");
+    QString name;
+    QString value;
+    QString typeId;
+    QString type;
+};
+
 class ParameterList : public QWidget
 {
     Q_OBJECT
 private:
     uint8_t _sysId = 0;
     uint8_t _compId = 0;
+    QTableWidget* _buffer;
+    QString _prevEdit = "";
+
+private:
+    void hideAll();
+    void showAll();
 public:
     explicit ParameterList(QWidget *parent = nullptr);
     ~ParameterList();
@@ -41,6 +64,12 @@ private slots:
     void on_syncVehicleWithUs_clicked();
     void on_syncUsWithVehicle_clicked();
 
+    void on_saveToFileButton_clicked();
+
+    void on_loadFromFileButton_clicked();
+
+    void on_lineEdit_textChanged(const QString &arg1);
+
 public slots:
     void onAutopilotHeartbeat(const mavlink_message_t& msg);
     void handleMavlink(const mavlink_param_value_t& msg);
@@ -48,6 +77,9 @@ public slots:
 
     void setSingleParameterRequested(size_t rowIndex);
     void setAllParametersRequested();
+
+    QString serializeParameter(size_t rowIndex);
+    void saveToFile(const QString& path);
 private:
     Ui::ParameterList *ui;
 };
