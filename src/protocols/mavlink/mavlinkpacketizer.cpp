18d94e104e1d05ff883528be7a4fc9fa11aecd52
asuart <b.kuchuk@csky.space>
2025-12-17 14:03:25 +0300

    fixed memcpy in onPushByte

--- src/protocols/mavlink/mavlinkpacketizer.cpp
+++ src/protocols/mavlink/mavlinkpacketizer.cpp
@@ -26,17 +26,10 @@ void MavlinkPacketizer::onSendMessageRequest(Message msg) {
 bool MavlinkPacketizer::onPushByte(uint8_t byte) {
     bool isSuccessfulyParsed = mavlink_parse_char(MAVLINK_COMM_0, byte, _msg, _status);
     if (isSuccessfulyParsed) {
-        Message msg(
-                    [](void* msg){
-                        mavlink_message_t* msgTyped = reinterpret_cast<mavlink_message_t*>(msg);
-                        mavlink_message_t* result = nullptr;
-                        std::memcpy(result, msgTyped, sizeof(mavlink_message_t));
-                        return reinterpret_cast<void*>(result);
-                    },
-                    [](void* msg){delete reinterpret_cast<mavlink_message_t*>(msg);},
-                    reinterpret_cast<void*>(_msg)
-        );
+        Message msg;
+        msg.write(_msg);
         emit messageReceived(msg);
+        delete _msg;
         _msg = new mavlink_message_t{};
         delete _status;
         _status = new mavlink_status_t{};
